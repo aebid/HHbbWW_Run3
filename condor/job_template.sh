44903826134a9ba55564ee20fb88962fffc2cfd3
aebid <aebid@tamu.edu>
2023-03-09 22:28:02 +0100

    Fixed to export the proxy in the python directory, allows xrootd to work now to remove xrdcp step

--- condor/job_template.sh
+++ condor/job_template.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-export X509_USER_PROXY=$1
-
 list_of_files=("test0.root" "test1.root")
 
 cd python
+export X509_USER_PROXY=../$1
 
 for i in ${!list_of_files[@]}
 do
     echo 'file is '
     echo ${list_of_files[$i]}
-    xrdcp root://cms-xrd-global.cern.ch//${list_of_files[$i]} temp_file_${i}.root
     ls -lh
-    python3 run_bbWW_processing.py -i temp_file_${i}.root -o ../condor_output_${i}.root -t 0 -d 0
-    rm temp_file_${i}.root
+    python3 run_bbWW_processing.py -i root://cms-xrd-global.cern.ch//${list_of_files[$i]} -o ../condor_output_${i}.root -t 0 -d 0
 done
 
 cd ..
 ls -lh
-hadd condor_output.root *condor_output_*.root
 
 echo 'Done!'
