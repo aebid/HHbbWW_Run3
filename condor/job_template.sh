d3fdbe81da2413787913b62eabc463f1a54548cc
aebid <aebid@tamu.edu>
2023-03-11 13:09:01 +0100

    Updated to handle multiple files in submission

--- condor/job_template.sh
+++ condor/job_template.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 list_of_files=("test0.root" "test1.root")
+filename=("filename.sh")
 
 cd python
 export X509_USER_PROXY=../$1
 
-for i in ${!list_of_files[@]}
-do
-    echo 'file is '
-    echo ${list_of_files[$i]}
-    ls -lh
-    python3 run_bbWW_processing.py -i root://cms-xrd-global.cern.ch//${list_of_files[$i]} -o ../condor_output_${i}.root -t 0 -d 0
-done
+
+ls -lh
+python3 run_bbWW_processing.py -i ${list_of_files} -o ../out_condor_${filename}.root -t 0 -d 0
 
 cd ..
 ls -lh
--- condor/make_condor_jobs.py
+++ condor/make_condor_jobs.py
@@ -3,7 +3,10 @@ import math
 
 project_folder = "TTBar"
 file_list = ["/store/mc/Run3Winter22NanoAOD/TTTo2L2Nu_CP5_13p6TeV_powheg-pythia8/NANOAODSIM/122X_mcRun3_2021_realistic_v9-v1/40000/0db470d2-1dfb-471e-b8b0-60fe5cad9ddf.root", "/store/mc/Run3Winter22NanoAOD/TTTo2L2Nu_CP5_13p6TeV_powheg-pythia8/NANOAODSIM/122X_mcRun3_2021_realistic_v9-v1/40000/1e132652-55fb-4732-a077-2e1b013c6fc3.root", "/store/mc/Run3Winter22NanoAOD/TTTo2L2Nu_CP5_13p6TeV_powheg-pythia8/NANOAODSIM/122X_mcRun3_2021_realistic_v9-v1/40000/35c1c54d-4500-4542-9fb4-51baa6d04083.root"]
-nFilesPerJob = 1
+
+project_folder = "data"
+file_list = ["/store/data/Run2022C/DoubleMuon/NANOAOD/PromptNanoAODv10_v1-v1/50000/aa5a4b71-fd45-45d9-bf26-ea4f2dc42882.root"]
+nFilesPerJob = 2
 
 nJobs = math.ceil(len(file_list)/nFilesPerJob)
 remaining_files = file_list
@@ -25,11 +28,14 @@ for job_count in range(nJobs):
         if "list_of_files=" in line:
             files_for_this_job = remaining_files[:nFilesPerJob]
             remaining_files = remaining_files[nFilesPerJob:]
-            string_to_write = 'list_of_files=('
+            string_to_write = 'list_of_files=("'
             for fname in files_for_this_job:
-                string_to_write = string_to_write + '"{}" '.format(fname)
-            string_to_write = string_to_write[:-1] + ')'
+                string_to_write = string_to_write + 'root://cms-xrd-global.cern.ch//{} '.format(fname)
+            string_to_write = string_to_write[:-1] + '")\n'
             job_file.write(string_to_write)
+        elif "filename=" in line:
+            filename = "job{}.sh".format(job_count)
+            job_file.write('filename=("{}")\n'.format(filename))
         else:
             job_file.write(line)
 
