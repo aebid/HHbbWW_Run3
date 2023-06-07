aa8afb35b8361e4c19aa2283d2823a6e282aa154
Raul Rabadan <raul.iraq.rabadan.trejo@cern.ch>
2023-06-07 17:19:38 +0200

    wip-include-mctruth; add mctruth signal to the event tree

--- python/genparticles.py
+++ python/genparticles.py
@@ -20,7 +20,7 @@ def find_genpart(genpart, pdgid, ancestors):
     """
 
     def check_id(p):
-        return np.abs(genpart.GenPart_pdgId) == p
+        return np.abs(genpart.pdgId) == p
 
     pid = reduce(np.logical_or, map(check_id, pdgid))
 
@@ -28,19 +28,68 @@ def find_genpart(genpart, pdgid, ancestors):
         ancs, ancs_idx = [], []
         for i, mother_id in enumerate(ancestors):
             if i == 0:
-                mother_idx = genpart[pid].GenPart_genPartIdxMother
+                mother_idx = genpart[pid].genPartIdxMother
             else:
-                mother_idx = genpart[ancs_idx[i-1]].GenPart_genPartIdxMother
-            ancs.append(np.abs(genpart[mother_idx].GenPart_pdgId) == anc_id)
-            anc_idx.append(mother_idx)
+                mother_idx = genpart[ancs_idx[i-1]].genPartIdxMother
+            ancs.append(np.abs(genpart[mother_idx].pdgId) == mother_id)
+            ancs_idx.append(mother_idx)
 
         decaymatch =  reduce(np.logical_and, ancs)
         return genpart[pid][decaymatch]
 
     return genpart[pid]
 
-def genpart_single_lepton(EventProcessor):
-    pass
+def single_lepton_genpart(EventProcess):
+    events = EventProcess.events
+    genparts = events.GenPart
+    
+    # b-quarks from Higgs decay
+    bFromH = find_genpart(genparts, [5], [25])
+    
+    # H -> WW
+    # light quarks W decay
+    qFromW = find_genpart(genparts, [1, 2 ,3, 4], [24, 25])
+    
+    # leptons from W decay
+    lepFromW = find_genpart(genparts, [11, 13, 15], [24, 25])
+    nuFromW = find_genpart(genparts, [12, 14, 16], [24, 25])
 
-def genpart_double_lepton(EventProcessor):
-    pass
+    events = ak.with_field(events, bFromH.pt, "bFromH_pt")
+    events = ak.with_field(events, bFromH.eta, "bFromH_eta")
+    events = ak.with_field(events, bFromH.phi, "bFromH_phi")
+    
+    events = ak.with_field(events, qFromW.pt, "qFromW_pt")
+    events = ak.with_field(events, qFromW.eta, "qFromW_eta")
+    events = ak.with_field(events, qFromW.phi, "qFromW_phi")
+
+    events = ak.with_field(events, lepFromW.pt, "lepFromW_pt")
+    events = ak.with_field(events, lepFromW.eta, "lepFromW_eta")
+    events = ak.with_field(events, lepFromW.phi, "lepFromW_phi")
+    
+    events = ak.with_field(events, nuFromW.pt, "nuFromW_pt")
+    events = ak.with_field(events, nuFromW.eta, "nuFromW_eta")
+    events = ak.with_field(events, nuFromW.phi, "nuFromW_phi")
+
+def double_lepton_genpart(EventProcess):
+    events = EventProcess.events
+    genparts = events.GenPart
+    
+    # b-quarks from Higgs decay
+    bFromH = find_genpart(genparts, [5], [25])
+    
+    # H -> WW
+    # leptons from W decay
+    lepFromW = find_genpart(genparts, [11, 13, 15], [24, 25])
+    nuFromW = find_genpart(genparts, [12, 14, 16], [24, 25])
+
+    events = ak.with_field(events, bFromH.pt, "bFromH_pt")
+    events = ak.with_field(events, bFromH.eta, "bFromH_eta")
+    events = ak.with_field(events, bFromH.phi, "bFromH_phi")
+    
+    events = ak.with_field(events, lepFromW.pt, "lepFromW_pt")
+    events = ak.with_field(events, lepFromW.eta, "lepFromW_eta")
+    events = ak.with_field(events, lepFromW.phi, "lepFromW_phi")
+    
+    events = ak.with_field(events, nuFromW.pt, "nuFromW_pt")
+    events = ak.with_field(events, nuFromW.eta, "nuFromW_eta")
+    events = ak.with_field(events, nuFromW.phi, "nuFromW_phi")
