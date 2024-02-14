38a13e2e5ef6ab7b7358158c70ad4a52c99ac54f
Devin <aebid@tamu.edu>
2024-02-14 12:11:01 +0100

    Added nEvts arg to the example call

--- python/call_run_example.sh
+++ python/call_run_example.sh
@@ -6,4 +6,4 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc13-opt/setup.sh
 
 #python3 -i run_bbWW_processing.py -i GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M-1400_testfile.root -o test.root -ry 2022 -MC 1 -t 0 -d 0 -XS 1.0 -SF 0 -HLTCut 1
 
-python3 -i run_bbWW_processing.py -i GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M-450_testfile.root -o GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M450_Run3Sync.root -ry 2022 -re D -MC 1 -t 0 -d 0 -XS 1.0 -SF 0 -HLTCut 0
+python3 -i run_bbWW_processing.py -i GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M-450_testfile.root -o GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M450_Run3Sync.root -ry 2022 -re D -MC 1 -t 0 -d 0 -XS 1.0 -SF 0 -HLTCut 0 -nEvts 250000
