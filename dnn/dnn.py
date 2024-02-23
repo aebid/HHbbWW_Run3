0d1fe93c32fc4b45023e819a5f19ca1550e24c8d
aebid <aebid@tamu.edu>
2024-02-23 14:44:56 +0100

    Added ST class and more metrics (ROC curve)

--- dnn/dnn.py
+++ dnn/dnn.py
@@ -6,14 +6,23 @@ import uproot
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+import sklearn.metrics
+
+
+
+
 print(tf.__version__)
+
+tf.keras.backend.clear_session()
 
 
 signal_sample = "GluGlutoRadion_2L_M450_PreEE.root"
 signal_file = uproot.open(signal_sample)
 signal_tree = signal_file['Double_Tree']
 signal_events = signal_tree.arrays()
-signal_resolved2b_events = signal_events[signal_events.Double_Res_2b == 1]
+signal_resolved2b_events = signal_events[(signal_events.Double_Res_2b == 1) & (signal_events.Double_Signal == 1) & (signal_events.ZMassCut == 1) & (signal_events.InvarMassCut == 1)]
 signal_events_filtered = signal_resolved2b_events
 
 
@@ -21,7 +30,7 @@ tt_sample = "TTto2L2Nu_PreEE.root"
 tt_file = uproot.open(tt_sample)
 tt_tree = tt_file['Double_Tree']
 tt_events = tt_tree.arrays()
-tt_resolved2b_events = tt_events[tt_events.Double_Res_2b == 1]
+tt_resolved2b_events = tt_events[(tt_events.Double_Res_2b == 1) & (tt_events.Double_Signal == 1) & (tt_events.ZMassCut == 1) & (tt_events.InvarMassCut == 1)]
 tt_events_filtered = tt_resolved2b_events
 
 
@@ -29,9 +38,15 @@ DY_sample = "DYto2L-2Jets_MLL-10to50_PreEE.root"
 DY_file = uproot.open(DY_sample)
 DY_tree = DY_file['Double_Tree']
 DY_events = DY_tree.arrays()
-DY_resolved2b_events = DY_events[DY_events.Double_Res_2b == 1]
+DY_resolved2b_events = DY_events[(DY_events.Double_Res_2b == 1) & (DY_events.Double_Signal == 1) & (DY_events.ZMassCut == 1) & (DY_events.InvarMassCut == 1)]
 DY_events_filtered = DY_resolved2b_events
 
+ST_sample = "TbarWplusto2L2Nu_PreEE.root"
+ST_file = uproot.open(ST_sample)
+ST_tree = ST_file['Double_Tree']
+ST_events = ST_tree.arrays()
+ST_resolved2b_events = ST_events[(ST_events.Double_Res_2b == 1) & (ST_events.Double_Signal == 1) & (ST_events.ZMassCut == 1) & (ST_events.InvarMassCut == 1)]
+ST_events_filtered = ST_resolved2b_events
 
 
 
@@ -39,21 +54,25 @@ print("After filtering for only Double_Res_2b, we have these events left")
 print("Signal ", len(signal_events_filtered))
 print("TT ", len(tt_events_filtered))
 print("DY ", len(DY_events_filtered))
+print("ST ", len(ST_events_filtered))
 print("Starting the training and then will test")
 
 
 #Lets try to use class weights? This allows us to have a weight per class to emphasize underrepresented data in the training
-total_events = len(signal_events_filtered) + len(tt_events_filtered) + len(DY_events_filtered)
+total_events = len(signal_events_filtered) + len(tt_events_filtered) + len(DY_events_filtered) + len(ST_events_filtered)
 signal_classweight = 1.0/len(signal_events_filtered)
 tt_classweight = 1.0/len(tt_events_filtered)
 DY_classweight = 1.0/len(DY_events_filtered)
-class_weight = {0: signal_classweight, 1: tt_classweight, 2: DY_classweight}
+ST_classweight = 1.0/len(ST_events_filtered)
+class_weight = {0: signal_classweight, 1: tt_classweight, 2: DY_classweight, 3: ST_classweight}
+class_labels = ["Signal", "TT", "DY", "ST"]
 print("Using classweights, weights are")
 print(class_weight)
+print(class_labels)
+#Manuel has code fo better classweights, input plotting, and something else ask him he will send code
 
 #Turn the labels into onehots, prepare the binarizer
-from sklearn.preprocessing import LabelBinarizer
-label_binarizer = LabelBinarizer().fit([0,1,2])
+label_binarizer = LabelBinarizer().fit([0,1,2,3])
 
 
 #Put in a list of [ [px, py, pz, E, ...], [px, py, pz, E, ...], [px, py, pz, E, ...] ]
@@ -85,13 +104,72 @@ def create_nparray(events):
         events.ak4_jet1_E,
         events.ak4_jet1_btagDeepFlavB,
 
+        events.ak4_jet2_px,
+        events.ak4_jet2_py,
+        events.ak4_jet2_pz,
+        events.ak4_jet2_E,
+        events.ak4_jet2_btagDeepFlavB,
+
+        events.ak4_jet3_px,
+        events.ak4_jet3_py,
+        events.ak4_jet3_pz,
+        events.ak4_jet3_E,
+        events.ak4_jet3_btagDeepFlavB,
+
         events.met_px,
         events.met_py,
         events.met_pz,
         events.met_E,
 
+        events.HT,
+
+        events.ll_px,
+        events.ll_py,
+        events.ll_pz,
+        events.ll_E,
+        events.ll_mass,
+
+        events.hWW_px,
+        events.hWW_py,
+        events.hWW_pz,
+        events.hWW_E,
+        events.hWW_mass,
+
+        events.hbb_px,
+        events.hbb_py,
+        events.hbb_pz,
+        events.hbb_E,
+        events.hbb_mass,
+
+        events.hh_px,
+        events.hh_py,
+        events.hh_pz,
+        events.hh_E,
+        events.hh_mass,
+
+        events.n_fakeable_muons,
+        events.n_fakeable_electrons,
+        events.n_cleaned_ak4_jets,
+        events.n_medium_btag_ak4_jets,
+
+        #For now we must do dR by hand (not in tree)
+        ((events.lep0_eta - events.lep1_eta)**2 + (events.lep0_phi - events.lep1_phi)**2)**(0.5),
+        ((events.ak4_jet0_eta - events.ak4_jet1_eta)**2 + (events.ak4_jet0_phi - events.ak4_jet1_phi)**2)**(0.5)
+
     ])
     return array
+
+"""
+#Test simple inputs to see that resutls make sense (cannot classify)
+def create_nparray(events):
+    array = np.array([
+        events.met_px,
+        events.met_py,
+        events.met_pz,
+        events.met_E,
+    ])
+    return array
+"""
 
 signal_array = create_nparray(signal_events_filtered)
 signal_array = signal_array.transpose()
@@ -105,95 +183,62 @@ DY_array = create_nparray(DY_events_filtered)
 DY_array = DY_array.transpose()
 DY_label = np.full(len(DY_array), 2)
 
+ST_array = create_nparray(ST_events_filtered)
+ST_array = ST_array.transpose()
+ST_label = np.full(len(ST_array), 3)
+
+#How many inputs?
+input_len = len(signal_array[0])
 
 
-
-#Loaded all of the events, now we separate what we want to train on and what we want to test on
-#Will use 90% of events for training, 10% for testing
-PercOfTrain = 0.5
-nSignalTrain = int(PercOfTrain*len(signal_events_filtered))
-nttTrain = int(PercOfTrain*len(tt_events_filtered))
-nDYTrain = int(PercOfTrain*len(DY_events_filtered))
-
-
-train_signal_events = signal_array[:][:nSignalTrain]
-train_signal_labels = signal_label[:][:nSignalTrain]
-
-test_signal_events = signal_array[:][nSignalTrain:]
-test_signal_labels = signal_label[:][nSignalTrain:]
-
-
-train_tt_events = tt_array[:][:nttTrain]
-train_tt_labels = tt_label[:][:nttTrain]
-
-test_tt_events = tt_array[:][nttTrain:]
-test_tt_labels = tt_label[:][nttTrain:]
-
-
-train_DY_events = DY_array[:][:nDYTrain]
-train_DY_labels = DY_label[:][:nDYTrain]
-
-test_DY_events = DY_array[:][nDYTrain:]
-test_DY_labels = DY_label[:][nDYTrain:]
-
-
-
-
-train_set = np.concatenate((train_signal_events, train_tt_events, train_DY_events))
-train_labels = np.concatenate((train_signal_labels, train_tt_labels, train_DY_labels))
-
-
-
-#Shuffle the train set and labels in unison
-shuffle_index = np.random.permutation(len(train_signal_events)+len(train_tt_events)+len(train_DY_events))
-
-train_set = train_set[shuffle_index]
-train_labels = train_labels[shuffle_index]
-train_labels_onehot = label_binarizer.transform(train_labels)
-
-
-
-test_set = np.concatenate((test_signal_events, test_tt_events, test_DY_events))
-test_labels = np.concatenate((test_signal_labels, test_tt_labels, test_DY_labels))
-
-#Shuffle the train set and labels in unison
-shuffle_index = np.random.permutation(len(test_signal_events)+len(test_tt_events)+len(test_DY_events))
-
-test_set = test_set[shuffle_index]
-test_labels = test_labels[shuffle_index]
-test_labels_onehot = label_binarizer.transform(test_labels)
+#Prepare train and test samples, as well as random states
+events = np.concatenate((signal_array, tt_array, DY_array, ST_array))
+labels = np.concatenate((signal_label, tt_label, DY_label, ST_label))
+events_train, events_test, labels_train, labels_test = train_test_split(events, labels, test_size=0.33, random_state=42)
+labels_train = label_binarizer.transform(labels_train)
+labels_test = label_binarizer.transform(labels_test)
 
 
 #Create a layer to normalize the inputs
 #Inputs for DNN must be normalized, set normalization funcs on the train set
 norm_inputs = tf.keras.layers.Normalization(axis=-1)
-norm_inputs.adapt(train_set)
+norm_inputs.adapt(events_train)
 
-train_set_norm = norm_inputs(train_set)
-test_set_norm = norm_inputs(test_set)
+
+events_train_norm = norm_inputs(events_train)
+events_test_norm = norm_inputs(events_test)
+
+print("Train before norm")
+print(events_train)
+print("Train after norm")
+print(events_train_norm)
 
 
 
 model = tf.keras.Sequential()
 
-#For resolved case, we start with Lep1/2 and Jet1/2 4 vectors, E,px,py,pz
-#Since these will already be flattened (not 2D) we do not need a flatten layer
+#Manuel recommends having the nodes look like a cone and continue to decrease
+model.add(tf.keras.layers.Dense(128, input_dim=input_len))
 
-model.add(tf.keras.layers.Dense(26, activation="relu"))
-
-model.add(tf.keras.layers.Dense(64, activation="relu"))
-
-model.add(tf.keras.layers.Dropout(0.1))
+model.add(tf.keras.layers.Dropout(0.3))
 
 model.add(tf.keras.layers.Dense(64, activation="relu"))
 
-model.add(tf.keras.layers.Dropout(0.1))
+model.add(tf.keras.layers.Dropout(0.3))
 
-model.add(tf.keras.layers.Dense(64, activation="relu"))
+model.add(tf.keras.layers.Dense(32, activation="relu"))
 
-model.add(tf.keras.layers.Dropout(0.1))
+model.add(tf.keras.layers.Dropout(0.3))
 
-model.add(tf.keras.layers.Dense(3, activation="softmax"))
+model.add(tf.keras.layers.Dense(16, activation="relu"))
+
+model.add(tf.keras.layers.Dropout(0.3))
+
+model.add(tf.keras.layers.Dense(8, activation="relu"))
+
+model.add(tf.keras.layers.Dropout(0.3))
+
+model.add(tf.keras.layers.Dense(4, activation="softmax"))
 
 model.compile(
     optimizer='adam',
@@ -208,33 +253,56 @@ model.compile(
 
 
 
-#model.fit(train_set, train_labels, epochs=10, class_weight=class_weight)
-history = model.fit(train_set_norm, train_labels_onehot, epochs=10, class_weight=class_weight)
+history = model.fit(events_train_norm, labels_train, validation_data=(events_test_norm, labels_test), epochs=200, class_weight=class_weight, batch_size=4096)
 
 prob_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
-prob_examples = prob_model.predict(test_set_norm)
+prob_examples = prob_model.predict(events_test_norm)
 
-#test_loss, test_acc = model.evaluate(test_set, test_labels, verbose=2)
-test_loss, test_acc = model.evaluate(test_set_norm, test_labels_onehot, verbose=2)
-
+#test_loss, test_acc = model.evaluate(test_set_norm, test_labels_onehot, verbose=2)
 
 
 
 
-import sklearn.metrics
-
-cm = sklearn.metrics.confusion_matrix(np.argmax(test_labels_onehot, axis=1), np.argmax(prob_examples, axis=1), normalize='true')
-disp = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=["Signal", "TT", "DY"])
+cm = sklearn.metrics.confusion_matrix(np.argmax(labels_test, axis=1), np.argmax(prob_examples, axis=1), normalize='true')
+disp = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_labels)
 disp.plot(cmap=plt.cm.Blues)
 plt.savefig("conf_matrix.pdf")
 plt.close()
 
 #Lets look at training metrics, maybe just a plot of the history?
 plt.plot(history.history['categorical_accuracy'])
+plt.plot(history.history['val_categorical_accuracy'])
+plt.grid(True)
+plt.xlabel("Epoch")
+plt.ylabel("Categorical Accuracy")
+plt.ylim(0, 1.1)
+plt.legend(['train', 'test'], loc='upper left')
 plt.savefig("accuracy.pdf")
 plt.close()
 
 plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.grid(True)
+plt.yscale("log")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend(['train', 'test'], loc='upper left')
 plt.savefig("loss.pdf")
 plt.close()
+
+
+
+#Lets put the ROC curve for each class on a plot and calculate AUC
+g, c_ax = plt.subplots(1,1, figsize = (12,8))
+for (idx, c_label) in enumerate(class_labels):
+    fpr, tpr, thresholds = sklearn.metrics.roc_curve(labels_test[:,idx].astype(int), prob_examples[:,idx])
+    c_ax.plot(fpr, tpr, label = "{label} (AUC: {auc})".format(label = c_label, auc = sklearn.metrics.auc(fpr, tpr)))
+
+c_ax.plot(fpr, fpr, "b-", label = "Random Guessing")
+
+c_ax.legend()
+c_ax.grid(True)
+c_ax.set_xlabel("False Positive Rate")
+c_ax.set_ylabel("True Positive Rate")
+g.savefig("roc_curves.pdf")
