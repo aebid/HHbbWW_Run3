647727ef290893246803b4178cc82c718ae9060b
aebid <aebid@tamu.edu>
2024-03-15 20:15:09 +0100

    Fixed typos in args

--- hme/call_hme_example.sh
+++ hme/call_hme_example.sh
@@ -2,4 +2,4 @@
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc13-opt/setup.sh
 
-python3 -i run_hme.py -i GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M-450_testfile.root -o GluGlutoRadiontoHHto2B2Vto2B2L2Nu_M450_HME.root -it 100 -doSL 0 -doDL 1 -debug 0
+python3 -i run_hme.py -i input.root -o output.root -it 100 -SL 0 -DL 1 -d 0
--- hme/run_hme.py
+++ hme/run_hme.py
@@ -5,9 +5,9 @@ parser = argparse.ArgumentParser(description='HeavyMassEstimator for H->hh->bbWW
 parser.add_argument("-i", "--inputFile", dest="infile", type=str, default=[], help="input file name. [Default: [] ]")
 parser.add_argument("-o", "--outputFile", dest="outfile", type=str, default="out.root", help="output file name. [Default: 'out.root']")
 parser.add_argument("-it", "--iterations", dest="iterations", type=int, default=10000, help="number of iterations [Default: 10000]")
-parser.add_argument("-SL", "--singlelepton", dest="doSL", type=bool, default=1, help="do single lepton HME [Default: 1]")
-parser.add_argument("-DL", "--doublelepton", dest="doDL", type=bool, default=1, help="do double lepton HME [Default: 1]")
-parser.add_argument("-d", "--debug", dest="debug", type=bool, default=0, help="debug [Default: 0]")
+parser.add_argument("-SL", "--singlelepton", dest="doSL", type=int, default=1, help="do single lepton HME [Default: 1]")
+parser.add_argument("-DL", "--doublelepton", dest="doDL", type=int, default=1, help="do double lepton HME [Default: 1]")
+parser.add_argument("-d", "--debug", dest="debug", type=int, default=0, help="debug [Default: 0]")
 args, unknown = parser.parse_known_args()
 
 fname = args.infile
